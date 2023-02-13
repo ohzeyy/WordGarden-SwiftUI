2cd4d980f31be9318ebf3be75aa37521ec514845
Zach Peter <peterz@bc.edu>
2023-02-13 13:40:52 -0500

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Zach Peter on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
